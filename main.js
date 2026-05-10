ae47c38499e978baaa038735da3d74ee01e715c2
wilbert <wilbertgescobar48@gmail.com>
2026-05-10 02:51:57 -0500

    actualizado

--- main.js
+++ main.js
@@ -1,11 +1,46 @@
+import { resolve } from 'node:dns';
 import { createServer } from 'node:http';
 
 const server = createServer((req, res) => {
-  console.log(req); // Evita loguear 'req' entero porque es un objeto gigante
-  res.end("hola mundo");
-}); // Aquí faltaba cerrar la llave y el paréntesis
+  const { method, url } = req;
 
-// Tienes que decirle al servidor en qué puerto escuchar
+if (method === 'GET' && url === '/') {
+   res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end ('hola mundo este es el home y algo mas');
+  return;
+}
+
+//ruta de la api
+if (method === 'GET' && url === '/api') {
+   res.writeHead(200, {"content-type": "application/json"});
+  res.end (JSON.stringify({ message: 'respuesta en json', code: 200, status: 'ok' }));
+  return;
+}
+
+//ruta de health
+if (method === 'GET' && url === '/health') {
+    res.writeHead(200, {"content-type": "application/json"});
+    res.end(JSON.stringify({ message: 'mensaje en json desde health', code: 200, status: 'ok' }));
+    return;
+}
+  res.end("hola mundo");
+}); 
+
+/*const listen = (server, port) => {
+  return new Promise((resolve, reject) => {
+    server.listen(port, () => resolve('todo fue bien'));
+  });
+};
+
+listen(server, 3000).then((mensaje) => {
+  console.log(mensaje);
+});*/
+await new Promise((resolve) => server.listen(3000, resolve));
+/*
 server.listen(3000, () => {
+  if (err) {
+    console.error(err);
+    return;
+  }
   console.log('el servidor va bien');
-});
+});*/
